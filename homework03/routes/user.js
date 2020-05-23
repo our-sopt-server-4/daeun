b446f42d6b7d9a51bb49ef714dda3702ceef8e3b
DaEunShim <10ekdms01@naver.com>
2020-05-23 22:41:19 +0900

    3차 과제 제출(level_1)

---
+++ homework03/routes/user.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+let responseMessage = require('../modules/responseMessage');
+let statusCode = require('../modules/statusCode');
+let util = require('../modules/util');
+let User = require('../models/user');
+
+router.post('/signin', async(req,res)=>{
+  const {id, password} = req.body;
+
+  // request data null 값 확인
+  if(!id || !password)
+    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,responseMessage.NULL_VALUE));
+
+  // 아이디 존재 확인
+  const user = User.filter(user => user.id == id);
+  if(user.length==0)
+    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,responseMessage.NO_USER));
+
+  // 비밀번호 확인
+  if(user[0].password !== password)
+    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,responseMessage.MISS_MATCH_PW));
+
+  // 성공
+  res.status(statusCode.OK).send(util.success(statusCode.OK,responseMessage.LOGIN_SUCCESS, {userId: id}));
+
+});
+
+router.get('/profile/:id',async(req,res)=>{
+  const id = req.params.id;
+
+  // 존재하지 않는 아이디
+  const user = User.filter(user => user.id == id);
+  if(user[0] === undefined)
+    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,responseMessage.NO_USER));
+
+  // 성공
+  res.status(statusCode.OK).send(util.success(statusCode.OK,responseMessage.LOGIN_SUCCESS, user[0]));
+
+});
+
+module.exports = router;
