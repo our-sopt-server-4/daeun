f3351260f923cc76dc4099279169ca86abb46941
DaEunShim <10ekdms01@naver.com>
2020-05-23 23:52:17 +0900

    3차 과제 제출- 수정

--- homework03/routes/user.js
+++ homework03/routes/user.js
@@ -61,7 +61,7 @@ router.get('/profile/:id',async(req,res)=>{
     res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,responseMessage.NO_USER));
 
   // 성공
-  res.status(statusCode.OK).send(util.success(statusCode.OK,responseMessage.LOGIN_SUCCESS, user[0]));
+  res.status(statusCode.OK).send(util.success(statusCode.OK,responseMessage.READ_PROFILE_SUCCESS, user[0]));
 
 });
 
